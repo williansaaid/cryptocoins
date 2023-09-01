0426ad9494caab84d9cc0d0ffe60f456190bc572
Willians Jimenez <williansaaid@hotmail.com>
2023-09-01 18:56:41 -0500

    Prep for start

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        first: '#040D12',
+        second: '#183D3D',
+        third: '#5C8374',
+        fourth: '#5C8374',
+      }
     },
   },
   plugins: [],
