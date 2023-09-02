0f063206ce12113db71f904f0d1ae494a1f4af4d
Willians Jimenez <williansaaid@hotmail.com>
2023-09-01 19:04:09 -0500

    deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,13 @@ export default function Home() {
   return (
     <>
       <main
-        className="flex flex-col min-h-screen w-full bg-white pt-8 md:pt-16 xl:pt-20 items-center"
+        className="flex flex-col min-h-screen w-full bg-fourth pt-8 md:pt-16 xl:pt-20 items-center justify-center"
       >
-        <p>Hola</p>
+        <p
+          className="text-xl font-bold"
+        >
+          Website under construction
+        </p>
       </main>
     </>
   )
